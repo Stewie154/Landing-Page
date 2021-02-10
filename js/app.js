5b76deb076911d2ecda3712b623edc04febb22df
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-02-10 10:12:56 +0000

    Nav Bar
    Here I've made the function to populate the navbar with 4 links. The function runs with the event listener domContentLoaded.
    
    I've changed the styling in css so that the nav links are spread evenly across the width of the container. I did this by making the <ul> element (which is housing the <li> elements a flex-container.

--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@
 */
 //container for when each navLink is populated
 const navLinksContainer = document.getElementById('navbar__list');
-//navLink left empty to be populated for each section by buildNav function 
-let navLink = ``;
+//navLinks left empty to be populated for each section by buildNav function 
+let navLinks = ``;
 //grab all sections in the html via the <section> tag
 const sections = document.getElementsByTagName('section');
 /**
@@ -40,12 +40,23 @@ const sections = document.getElementsByTagName('section');
 // build the nav
 
 const buildNav = () => { 
+    //iterating through each section to create each nav link
     for(section of sections) {
-        console.log('hello');
+        //getting id and data-nav(name of each section) for each section
+        sectionID = section.id;
+        navLinkData = section.dataset.nav;
+        
+        //Create and store nav links for each section in the navLinks variable. Each navLink is given a href from the sectionID variable, and a display name from the NavLinkData variable
+        navLinks += `<li class="menu__link"><a href="#${sectionID}">${navLinkData}</a></li>`;
     }
+
+    //adds the <li> elements inside the <ul>. I have made the <ul> a flex container in css, so the <li> elements are spaced evenly across the container
+    navLinksContainer.innerHTML = navLinks;
 };
 
-buildNav();
+//wait for dom to load before adding navbar elements
+document.addEventListener('domContentLoaded', buildNav());
+
 
 // Add class 'active' to section when near top of viewport
 
